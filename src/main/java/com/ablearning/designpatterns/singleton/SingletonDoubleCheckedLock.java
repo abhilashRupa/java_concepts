402f844f0f874286b9853a373a1e987325f72fe6
Abhilash Rupa <abhilashrupa@gmail.com>
2025-01-15 22:22:33 +0530

    singleton design pattern with dobele checked lock

--- src/main/java/com/ablearning/designpatterns/singleton/SingletonDoubleCheckedLock.java
+++ src/main/java/com/ablearning/designpatterns/singleton/SingletonDoubleCheckedLock.java
@@ -1,2 +1,41 @@
-package com.ablearning.designpatterns.singleton;public class SingletonDoubleCheckedLock {
+package com.ablearning.designpatterns.singleton;
+
+public class SingletonDoubleCheckedLock {
+	
+	public static void main(String[] args){
+		Thread t1 = new Thread(() -> {
+			LogManagerDoubleCheckedLock logManager1 = LogManagerDoubleCheckedLock.getLogManagerInstance();
+		});
+	
+	
+	Thread t2 = new Thread(() -> {
+		LogManagerDoubleCheckedLock logManager2 = LogManagerDoubleCheckedLock.getLogManagerInstance();
+	});
+	
+	t1.start();
+	t2.start();
+	
+	}
 }
+
+class LogManagerDoubleCheckedLock{
+	
+	public static LogManagerDoubleCheckedLock logManager;
+	
+	private LogManagerDoubleCheckedLock(){
+		System.out.println("LogManagerDoubleCheckedLock instance created");
+	}
+	
+	public static LogManagerDoubleCheckedLock getLogManagerInstance(){
+		if(logManager == null)
+			synchronized(LogManagerDoubleCheckedLock.class){
+				if(logManager == null)
+					logManager = new LogManagerDoubleCheckedLock();		
+			}
+				
+		return logManager;
+	}
+	
+}
+
+
