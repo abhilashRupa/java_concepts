c3e1ef61b13a36d6a3257ebb794d8ab0ad138901
Abhilash Rupa <abhilashrupa@gmail.com>
2025-01-15 22:18:36 +0530

    singleton design pattern with lazy loading

--- src/main/java/com/ablearning/designpatterns/singleton/SingletonLazyDemo.java
+++ src/main/java/com/ablearning/designpatterns/singleton/SingletonLazyDemo.java
@@ -1,2 +1,31 @@
-package com.ablearning.designpatterns.singleton;public class SingletonLazyDemo {
+package com.ablearning.designpatterns.singleton;
+
+public class SingletonLazyDemo {
+	public static void main(String[] args){
+		
+		LogManager logManager1 = LogManager.getLogManagerInstance();
+		LogManager logManager2 = LogManager.getLogManagerInstance();
+		
+		System.out.println(logManager1.hashCode());
+		System.out.println(logManager2.hashCode());
+		
+	}
+}
+
+class LogManager{
+	
+	static LogManager logManager;
+	
+	private LogManager(){
+		System.out.println("instance created.");
+	}
+	
+	public static LogManager getLogManagerInstance(){
+		if(logManager==null)
+			logManager = new LogManager();
+		return logManager;
+
+		
+	}
+	
 }
