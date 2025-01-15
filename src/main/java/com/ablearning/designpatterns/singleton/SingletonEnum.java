8e59fbb1b4e453ea50e309148aece6f3101ce7c1
Abhilash Rupa <abhilashrupa@gmail.com>
2025-01-15 22:27:50 +0530

    singleton design pattern with enum

--- src/main/java/com/ablearning/designpatterns/singleton/SingletonEnum.java
+++ src/main/java/com/ablearning/designpatterns/singleton/SingletonEnum.java
@@ -1,2 +1,28 @@
-package com.ablearning.designpatterns.singleton;public class SingletonEnum {
+package com.ablearning.designpatterns.singleton;
+
+public class SingletonEnum {
+	public static void main(String[] args){
+		
+			LogManagerEnum logManager1 = LogManagerEnum.INSTANCE;
+			LogManagerEnum logManager2 = LogManagerEnum.INSTANCE;
+			
+			System.out.println(logManager1.hashCode());
+			System.out.println(logManager2.hashCode());
+
+			logManager1.i = 10;
+			logManager1.show();
+
+			logManager2.i = 20;
+			logManager1.show();
+		
+	}
+}
+
+enum LogManagerEnum{
+	INSTANCE;
+
+	int i;
+	public void show(){
+		System.out.println(i);
+	}
 }
